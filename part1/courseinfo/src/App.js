d96101ff4c676678b57629b045b58ff3c4cdf59d
gutun <103473308+gutunn@users.noreply.github.com>
2022-11-15 22:09:32 +0800

    practice

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,66 +1,33 @@
+import { useState } from "react"
 
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course.name}</h1>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part p1={props.course.parts[0].name} e1={props.course.parts[0].exercises}/>
-      <Part p2={props.course.parts[1].name} e2={props.course.parts[1].exercises}/>
-      <Part p3={props.course.parts[2].name} e3={props.course.parts[2].exercises}/>
-    </div>
-  )
-  }
-
-const Part = (props) => {
-  return (
-    <div>
-      <p>{props.p1} {props.e1}</p>
-      <p>{props.p2} {props.e2}</p>
-      <p>{props.p3} {props.e3}</p>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
-    </div>
-  )
-}
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-    {
-      name: 'Fundamental of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-}
+  const [ counter, setCounter ] = useState(0)
 
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1) 
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <Header course={course}/>
-      <Content course={course}/>
-      <Total course={course}/>
+      <Display counter={counter}/>
+      <Button onClick={increaseByOne}
+        text='plus'
+      />
+      <Button onClick={setToZero}
+        text='zero'
+      />
+       <Button onClick={decreaseByOne}
+        text='minus'
+      />
     </div>
   )
 }
-export default App;
+
+const Display = ({ counter }) => <div>{counter}</div>
+  
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+  
+
+
+export default App
