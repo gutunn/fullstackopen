10bd90f96c5e5b122192d3967800b35b6c0cfb65
gutun <103473308+gutunn@users.noreply.github.com>
2022-11-16 23:09:00 +0800

    feature right left button

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,32 +2,19 @@ import { useState } from "react"
 
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1) 
-  const setToZero = () => setCounter(0)
-
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const handleLeftClick = () => {    setAll(allClicks.concat('L'))    setLeft(left + 1)  }
+  const handleRightClick = () => {    setAll(allClicks.concat('R'))    setRight(right + 1)  }
   return (
     <div>
-      <Display counter={counter}/>
-      <Button onClick={increaseByOne}
-        text='plus'
-      />
-      <Button onClick={setToZero}
-        text='zero'
-      />
-       <Button onClick={decreaseByOne}
-        text='minus'
-      />
-    </div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>    </div>
   )
 }
-
-const Display = ({ counter }) => <div>{counter}</div>
-  
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
-  
-
 
 export default App
