caab3381bf4d4ae9e7d76b80f3f99c1f2848a7fd
gutun <103473308+gutunn@users.noreply.github.com>
2022-11-16 23:09:57 +0800

    feature right left button

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,18 +2,26 @@ import { useState } from "react"
 
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const handleLeftClick = () => {    setAll(allClicks.concat('L'))    setLeft(left + 1)  }
-  const handleRightClick = () => {    setAll(allClicks.concat('R'))    setRight(right + 1)  }
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
+
+  const handleLeftClick = () => 
+    
+    setClicks({left: clicks.left + 1,...clicks})
+  
+
+  const handleRightClick = () => 
+    setClicks({...clicks, right: clicks.right + 1 })
+  
+
   return (
     <div>
-      {left}
+      {clicks.left}
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
-      {right}
-      <p>{allClicks.join(' ')}</p>    </div>
+      {clicks.right}
+    </div>
   )
 }
 
